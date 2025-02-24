09b8faad348cc2b30cb186c8c47fcbb0cb7cef08
Christian Schmitz <support@monkeybreadsoftware.de>
2025-02-24 13:06:31 +0100

    Update websocket.c
    fixed tabs

--- docs/examples/websocket.c
+++ docs/examples/websocket.c
@@ -59,11 +59,11 @@ static int recv_pong(CURL *curl, const char *expected_payload)
   const struct curl_ws_frame *meta;
   char buffer[256];
   CURLcode result = curl_ws_recv(curl, buffer, sizeof(buffer), &rlen, &meta);
-	while (result == CURLE_AGAIN)
-	{
-		Curl_wait_ms(1);
-		result = curl_ws_recv(curl, buffer, sizeof(buffer), &rlen, &meta);
-	}
+  while(result == CURLE_AGAIN)
+  {
+    Curl_wait_ms(1);
+    result = curl_ws_recv(curl, buffer, sizeof(buffer), &rlen, &meta);
+  }
   if(!result) {
     if(meta->flags & CURLWS_PONG) {
       int same = 0;
